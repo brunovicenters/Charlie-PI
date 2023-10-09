bfa2adfe90983a697b233c30bf170cfda5f8f62d
bruno <brunovicente.dev@gmail.com>
2023-10-08 23:54:37 -0300

    First commit

---
+++ src/templates/navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+    </div>
+</nav>
