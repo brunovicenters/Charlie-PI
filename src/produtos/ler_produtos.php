06f7a17dd9b891b1bac31cf9791c20cea41dc310
Bruno Vicente <brunovicente.dev@gmail.com>
2023-10-15 14:18:07 -0300

    Back to normal

--- src/produtos/ler_produtos.php
+++ src/produtos/ler_produtos.php
@@ -20,15 +20,6 @@ $pagNome = "Gerenciar produtos";
                 Aliquid minus repudiandae adipisci vero, cumque recusandae suscipit ut ad odit velit? Repellat nam dolorum, accusamus fuga commodi nesciunt dignissimos quod alias magnam delectus tempora quia ipsa vero sapiente perspiciatis.
                 Cum odio ex necessitatibus aspernatur nostrum expedita impedit quisquam iusto nesciunt, reiciendis consequuntur maiores ratione natus in illum quo sed, tempora ut fuga porro, qui debitis vero officia sunt? Qui?
             </div>
-            <div class="col-md-6 offset-md-3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus exercitationem, cum, quibusdam molestiae eveniet sapiente adipisci pariatur quo maiores repudiandae voluptas sunt eius corporis dolor saepe, ratione suscipit. Illo, quidem?
-                Iure quibusdam doloremque dolorum enim, id quam quo, quisquam voluptas ut obcaecati deserunt est animi maxime consectetur ipsam suscipit laudantium laborum quae illo eum placeat? Quis ut quos velit nostrum.
-                Ad ut ipsum quia reprehenderit optio tenetur illo sapiente libero laborum accusantium enim maxime tempora impedit autem praesentium nemo, aut obcaecati asperiores. Voluptas magni praesentium inventore sint repellat ut quas.
-                Consectetur adipisci dolor pariatur quisquam sint animi voluptatibus reiciendis esse, quasi expedita dolorum quos numquam a dolorem quia assumenda asperiores voluptates? Sed quis ullam neque eius commodi culpa exercitationem id.
-                asdsadas Reprehenderit nulla similique doloribus modi. Cumque debitis, ipsum architecto quas aspernatur quae ab voluptatum aut enim error quo facilis mollitia consequuntur accusantium dolorem recusandae repellat excepturi provident in ea animi.
-                Aliquid minus repudiandae adipisci vero, cumque recusandae suscipit ut ad odit velit? Repellat nam dolorum, accusamus fuga commodi nesciunt dignissimos quod alias magnam delectus tempora quia ipsa vero sapiente perspiciatis.
-                Cum odio ex necessitatibus aspernatur nostrum expedita impedit quisquam iusto nesciunt, reiciendis consequuntur maiores ratione natus in illum quo sed, tempora ut fuga porro, qui debitis vero officia sunt? Qui?
-            </div>
         </div>
     </div>
 </body>
