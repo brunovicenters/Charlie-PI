45036b019a70b652fd839d721e2c470c05c88bb8
Iggor Manfio <iggormanfio4@hotmail.com>
2023-10-19 20:16:49 -0300

    criei formulário + processo login

--- src/login/login.php
+++ src/login/login.php
@@ -1,11 +1,27 @@
+<?php
+$pagNome = "Login"
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<?php include "../templates/head.php"; ?>
+
 <body>
-    
+<h2>Login Administrador</h2>
+<form action="processa_login.php" method="post" >
+    <label  for="email">Email:</l class="form-label">
+        <input  type="text" name="email" id="email" required>
+        <label  for="password">Senha:</label>
+        <input  type="password" name="password" id="password" required>
+        <button type="submit" >Entrar</button>
+        <!-- Mensagem de erro -->
+        <?php if (isset($_GET['error'])) {
+            echo "<p class='text-danger mt-3'>O usuário ou a senha está incorreto</p>";
+        } ?>
+    </form>
 </body>
+
 </html>
+
+
+
---
+++ src/login/processa_login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once('../../conexao/conexao.php');
+
+$email = htmlspecialchars($_POST["email"]);
+$password = htmlspecialchars($_POST['password']);
+
+$sql = "SELECT * FROM administrador WHERE ADM_EMAIL = :email AND ADM_SENHA = :password 
+AND ADM_ATIVO = 1";
+
+$query = $pdo->prepare($sql);
+$query->bindParam(':email', $email, PDO::PARAM_STR);
+$query->bindParam(':password', $password, PDO::PARAM_STR);
+$query->execute();
+
+if ($query->rowCount() > 0) {
+    $_SESSION["admin_login"] = true;
+    header("Location:teste.php");
+    exit();
+
+} else {
+    header("Location:login.php?error");
+    exit();
+}
+?>
