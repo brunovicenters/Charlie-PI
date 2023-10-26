890b160eb8ccac8af0a0da0175bf4b37a7659314
bruno <brunovicente.dev@gmail.com>
2023-10-26 00:14:56 -0300

    Pequenos ajustes de texto e classes

--- src/login/login.php
+++ src/login/login.php
@@ -7,11 +7,11 @@ $pagNome = "Login"
 <?php include "../templates/head.php"; ?>
 <link rel="stylesheet" href="./../assets/login.css">
 
-<body>
+<body id="login">
     <div class="container">
-        <div class="row d-flex flex-column justify-content-center align-items-center bodyLogin">
-            <img class="logoCharlie mb-5" src="../assets/logoCharlie.png">
-            <h2 class="text-center">Login Administrador</h2>
+        <div class="row flex-column justify-content-center align-items-center containerLogin">
+            <img class="logoCharlie mb-3" src="../assets/logoCharlie.png">
+            <h2 class="h2 text-center z-1 mb-0">Login</h2>
 
             <div class="card bg-danger">
                 <div class="card-body">
@@ -20,10 +20,10 @@ $pagNome = "Login"
                         <input class="form-control col-md-12 mb-2" type="text" name="email" id="email" required>
                         <label class="form-label rounded-4 ps-2 pe-5" for="password">Senha:</label>
                         <input class="form-control mb-2" type="password" name="password" id="password" required>
-                        <button class="btn btn-success btn btn-light m-auto d-block " type="submit">Entrar</button>
+                        <button class="btn btn-success btn btn-light mx-auto d-block " type="submit">Entrar</button>
                         <!-- Mensagem de erro -->
                         <?php if (isset($_GET['error'])) {
-                            echo "<p class='text-danger mt-3'>O usuário ou a senha está incorreto</p>";
+                            echo "<p class='text-danger mt-3'>O usuário ou a senha estão incorretos</p>";
                         } ?>
                     </form>
                 </div>
