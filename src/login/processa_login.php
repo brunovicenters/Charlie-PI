bec6b6ead50f4bc44d564820ee93eef741fafd9f
bruno <brunovicente.dev@gmail.com>
2023-10-26 15:08:31 -0300

    Nome da tabela corrigido

--- src/login/processa_login.php
+++ src/login/processa_login.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require_once('../../conexao/conexao.php');
+require_once('./../../conexao/conexao.php');
 
 $email = htmlspecialchars($_POST["email"]);
 $password = htmlspecialchars($_POST['password']);
 
-$sql = "SELECT * FROM administrador WHERE ADM_EMAIL = :email AND ADM_SENHA = :password 
+$sql = "SELECT * FROM ADMINISTRADOR WHERE ADM_EMAIL = :email AND ADM_SENHA = :password 
 AND ADM_ATIVO = 1";
 
 $query = $pdo->prepare($sql);
@@ -17,7 +17,6 @@ if ($query->rowCount() > 0) {
     $_SESSION["admin_login"] = true;
     header("Location:./../home/home.php");
     exit();
-
 } else {
     header("Location:./login.php?error");
     exit();
