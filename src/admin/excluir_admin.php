545709daf431160394e189aff9763b33fb8b16e8
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-04 20:24:20 -0300

    First commit

---
+++ src/admin/excluir_admin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+// if (!isset($_SESSION["admin_logado"])) {
+//     header("Location:../login/login.php");
+//     exit();
+// }
+
+// Conexão com o Banco de Dados
+require_once "../../conexao/conexao.php";
+
+if (isset($_GET['id'])) {
+    $query = $pdo->prepare("SELECT * FROM ADMINISTRADOR WHERE ADM_ID=?");
+    $query->execute([$_GET['id']]);
+    $admin = $query->fetch(PDO::FETCH_ASSOC);
+
+    if (!$admin) {
+        header('Location:./ler_admin.php');
+        exit();
+    } else {
+        $query = $pdo->prepare('DELETE FROM ADMINISTRADOR WHERE ADM_ID = ?');
+        $query->execute([$_GET['id']]);
+        header('Location:./ler_admin.php');
+        exit();
+    }
+} else {
+    header('Location:./ler_admin.php');
+    exit();
+}
