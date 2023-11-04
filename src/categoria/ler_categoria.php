0297b43b5a17d3ead984ea5989f520c6f568fe72
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-04 20:22:45 -0300

    Tirado a deleção de categorias

--- src/categoria/ler_categoria.php
+++ src/categoria/ler_categoria.php
@@ -121,11 +121,11 @@ if (isset($_POST['search']) && !empty(trim($_POST['search']))) {
                                         <div class="modal-dialog">
                                             <div class="modal-content">
                                                 <div class="modal-header">
-                                                    <h1 class="modal-title fs-5" id="deleteModalLabel">Tem certeza que quer deletar a categoria?</h1>
+                                                    <h1 class="modal-title fs-5" id="deleteModalLabel">A deleção de categorias está indisponível.</h1>
                                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-footer">
-                                                    <a href="./excluir_categoria.php?id=<?= $categoria['CATEGORIA_ID'] ?>" type="btn" class="btn bg-danger text-white">Delete</a>
+                                                    <!-- <a href="./excluir_categoria.php?id=< ?=  $categoria['CATEGORIA_ID'] ?>" type="btn" class="btn bg-danger text-white">Delete</a> -->
                                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                                 </div>
                                             </div>
