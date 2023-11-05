9fbb11ffa1e1ddb9b08ac131e2e90baa9bed3b4a
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-05 16:10:55 -0300

    Corrigido ativo

--- src/admin/editar_admin.php
+++ src/admin/editar_admin.php
@@ -21,7 +21,7 @@ if (isset($_GET['id']) && $_SESSION['admin_id'] == $_GET['id']) {
             $nome = isset($_POST['nome']) ? htmlspecialchars($_POST["nome"]) : '';
             $email = isset($_POST['email']) ? filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL) : '';
             $senha = isset($_POST['senha']) ? htmlspecialchars($_POST["senha"]) : '';
-            $ativo = isset($_POST['ativo']) ? filter_input(INPUT_POST, 'ativo', FILTER_SANITIZE_NUMBER_INT) : '';
+            $ativo = isset($_POST['ativo']) ? 1 : 0;
             $imagem = isset($_POST['imagem']) ? filter_input(INPUT_POST, 'imagem', FILTER_SANITIZE_URL) : '';
 
             $query = $pdo->prepare('UPDATE ADMINISTRADOR SET ADM_NOME = :nome, ADM_email = :email, ADM_SENHA = :senha, ADM_ATIVO = :ativo, ADM_IMAGEM = :img WHERE ADM_ID = :id');
