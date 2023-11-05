7814d04d00dbb8e4aee0acd05917eba6093b79f9
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-05 17:07:25 -0300

    Mudado script para template

--- src/produtos/criar_produtos.php
+++ src/produtos/criar_produtos.php
@@ -117,17 +117,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </div>
     </div>
 
-    <script>
-        // Adiciona um novo campo de imagem URL.
-        function adicionarImagem() {
-            const containerImagens = document.getElementById('containerImagens');
-            const novoInput = document.createElement('input');
-            novoInput.classList.add('form-control', 'col-md-12', 'mt-2', 'mb-3');
-            novoInput.type = 'text';
-            novoInput.name = 'imagem[]';
-            containerImagens.appendChild(novoInput);
-        }
-    </script>
+    <!-- Adiciona um novo campo de URL para imagem -->
+    <script src="./../scripts/adicionarImagem.js"></script>
 
 </body>
 
