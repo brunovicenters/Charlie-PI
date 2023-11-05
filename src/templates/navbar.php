ee6e3184bc7cf64f7af3e48b366757b2db34727f
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-05 16:52:39 -0300

    Add foto adm ou foto padrão

--- src/templates/navbar.php
+++ src/templates/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand p-0 m-0" href="./../home/home.php"><img src="../assets/image/logoC.png" alt="" class="logoNav"></a>
+    <a class="navbar-brand p-0 m-0" href="./../home/home.php"><img src="../assets/image/logoC.png" alt="Logo Charlie Reduzida" class="logoNav"></a>
     <h1 class="h5 me-3 mb-0 fw-normal"><?= $pagNome; ?></h1>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <img src="../assets/image/fotoAdm.png" alt="" class="fotoAdm">
+        <img src="<?= $_SESSION["admin_img"] ? $_SESSION["admin_img"] : './../assets/image/fotoAdm.png' ?>" alt="Foto do admin" class="fotoAdm">
         <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><?= $_SESSION["admin_nome"] ?></h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
