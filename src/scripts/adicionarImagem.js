70168b7102eeba2c4acdc6ac13abe369f0093495
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-05 21:24:55 -0300

    Type para url

--- src/scripts/adicionarImagem.js
+++ src/scripts/adicionarImagem.js
@@ -2,7 +2,7 @@ function adicionarImagem() {
   const containerImagens = document.getElementById("containerImagens");
   const novoInput = document.createElement("input");
   novoInput.classList.add("form-control", "col-md-12", "mt-2", "mb-3");
-  novoInput.type = "text";
+  novoInput.type = "url";
   novoInput.name = "imagem[]";
   containerImagens.appendChild(novoInput);
 }
