5ff84ba9efc97c3d6b88418737e2be75e44966cb
Bruno Vicente <brunovicente.dev@gmail.com>
2023-11-10 10:04:16 -0300

    Add botão fechar + deletar campos

--- src/scripts/adicionarImagem.js
+++ src/scripts/adicionarImagem.js
@@ -1,8 +1,36 @@
+let containerNum = 1;
+
 function adicionarImagem() {
   const containerImagens = document.getElementById("containerImagens");
+  const newImg = document.createElement("div");
+  newImg.setAttribute("id", `newImg${containerNum}`);
+  newImg.classList.add(
+    "d-flex",
+    "position-relative",
+    "justify-content-between"
+  );
+  containerImagens.appendChild(newImg);
   const novoInput = document.createElement("input");
-  novoInput.classList.add("form-control", "col-md-12", "mt-2", "mb-3");
+  novoInput.classList.add("form-control", "col-md-12", "mt-2", "mb-3", "pe-5");
   novoInput.type = "url";
   novoInput.name = "imagem[]";
-  containerImagens.appendChild(novoInput);
+  newImg.appendChild(novoInput);
+  const novoBotao = document.createElement("button");
+  novoBotao.setAttribute("type", "button");
+  novoBotao.classList.add(
+    "btn",
+    "border-0",
+    "position-absolute",
+    "end-0",
+    "mt-2"
+  );
+  novoBotao.setAttribute("onclick", `fecharCampo(newImg${containerNum})`);
+  novoBotao.innerHTML = '<i class="bi bi-x-square"></i>';
+  newImg.appendChild(novoBotao);
+  containerNum++;
+}
+
+function fecharCampo(idCampo) {
+  const campoDeletar = document.getElementById(idCampo.id);
+  campoDeletar.remove();
 }
