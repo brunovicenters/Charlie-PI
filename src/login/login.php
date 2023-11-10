c94c8d57ed8c3acd41f8b8502f9b85e9f8c1f405
bruno <brunovicente.dev@gmail.com>
2023-11-10 20:44:46 -0300

    Adicionado toast template

--- src/login/login.php
+++ src/login/login.php
@@ -28,21 +28,12 @@ $pagNome = "Login"
         </div>
     </div>
     <!-- Mensagem de erro -->
-    <?php if (isset($_GET['error'])) { ?>
-        <button type="button" class="btn visually-hidden position-absolute" id="liveToastBtn"></button>
-
-        <!-- Toast Message -->
-        <div class="toast-container position-fixed bottom-0 end-0 p-3">
-            <div id="liveToast" class="toast align-items-center bg-danger text-white" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="d-flex">
-                    <div class="toast-body">
-                        E-mail ou senha inválidos!
-                    </div>
-                    <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
+    <?php if (isset($_GET['error'])) {
+        $bgClass = "bg-danger text-white";
+        $msg = "E-mail ou senha inválidos!";
+        include "./../templates/toast.php";
+    }
+    ?>
     <script src="../scripts/toast.js"></script>
 </body>
 
